2de1b270cbc50f2e6daf49fd1efc0d3a4900df8f
Ghufran <ghugluq2t@gmail.com>
2025-02-26 19:16:12 +0000

    Changed double to add 2 to the given number instead of multiplying by 2. It gave an error which I will show in the readme.

--- test_calculator.py
+++ test_calculator.py
@@ -5,7 +5,7 @@ def main():
 
 def test_double():
     assert double(2) == 4
-    assert double(3) == 6 #works perfectly, now I will test with an error in the original function (double).
+    assert double(3) == 6
 
 if __name__ == "__main__":
     main()
