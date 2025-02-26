87b4ae44733dd60635b7f5cc5fe9e7211496b50f
Ghufran <ghugluq2t@gmail.com>
2025-02-26 19:13:06 +0000

    double works perfectly, but i will next test what happens if double didn't work perfectly

--- test_calculator.py
+++ test_calculator.py
@@ -5,7 +5,7 @@ def main():
 
 def test_double():
     assert double(2) == 4
-    assert double(3) == 6
+    assert double(3) == 6 #works perfectly, now I will test with an error in the original function (double).
 
 if __name__ == "__main__":
     main()
