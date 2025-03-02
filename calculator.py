68b32990468c30cc94a23337119dfed80001659a
Ghufran <ghugluq2t@gmail.com>
2025-03-02 07:53:38 +0000

    Added testing strings

--- calculator.py
+++ calculator.py
@@ -1,9 +1,10 @@
 def main():
-    number = int(input("Enter a number to double.\n"))
+    number = input("Enter a number to double.\n")
     print(f"{number} doubled is {double(number)}")
 
 def double(number):
-    return number * 2
+    return int(number) * 2
 
 if __name__ == "__main__":
     main()
+
--- test_calculator.py
+++ test_calculator.py
@@ -16,8 +16,14 @@ if __name__ == "__main__":
 
 #with pytest:
 from calculator import double
+import pytest
 
-def test_double():
+def test_two():
     assert double(2) == 4
+def test_three():
     assert double(3) == 6
+def test_zero():
     assert double(0) == 0
+def test_str():
+    with pytest.raises(ValueError):
+        double("two")
