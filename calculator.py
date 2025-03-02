b60a470ec6dfa5c25cabe5ab4a5877137cd5e430
Ghufran <ghugluq2t@gmail.com>
2025-03-02 07:20:30 +0000

    Tested program with pytest

--- calculator.py
+++ calculator.py
@@ -3,7 +3,7 @@ def main():
     print(f"{number} doubled is {double(number)}")
 
 def double(number):
-    return number + 2
+    return number * 2
 
 if __name__ == "__main__":
     main()
--- test_calculator.py
+++ test_calculator.py
@@ -1,3 +1,6 @@
+
+#original testing without pytest:
+'''
 from calculator import double
 
 def main():
@@ -9,3 +12,12 @@ def test_double():
 
 if __name__ == "__main__":
     main()
+'''
+
+#with pytest:
+from calculator import double
+
+def test_double():
+    assert double(2) == 4
+    assert double(3) == 6
+    assert double(0) == 0
