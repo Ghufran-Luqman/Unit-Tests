51008cf34feb5fbdc7e3d3eb5d918bc85963ff4f
Ghufran Luqman <88330113+Ghufran-Luqman@users.noreply.github.com>
2025-03-04 12:00:13 +0000

    added 'withpytest' branch

--- test_calculator.py
+++ test_calculator.py
@@ -1,19 +1,3 @@
-
-#original testing without pytest:
-'''
-from calculator import double
-
-def main():
-    test_double()
-
-def test_double():
-    assert double(2) == 4
-    assert double(3) == 6
-
-if __name__ == "__main__":
-    main()
-'''
-
 #with pytest:
 from calculator import double
 import pytest
