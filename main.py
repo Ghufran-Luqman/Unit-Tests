0707d021e387344d548c86a604a3e0d7c649c869
Ghufran Luqman <88330113+Ghufran-Luqman@users.noreply.github.com>
2025-03-04 12:33:39 +0000

    tested basic fastapi app

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    number = input("Enter a number to double.\n")
+    print(f"{number} doubled is {double(number)}")
+
+def double(number):
+    return int(number) * 2
+
+if __name__ == "__main__":
+    main()
+
